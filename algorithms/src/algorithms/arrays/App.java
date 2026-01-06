e60bd05c2d5fe28d76d4c22c8ca21ad5be89226f
Jeon Seung Hwan <fabronjeon@naver.com>
2026-01-06 12:49:50 +0900

    Section4-Practice 1

--- algorithms/src/algorithms/arrays/App.java
+++ algorithms/src/algorithms/arrays/App.java
@@ -1,33 +1,34 @@
 package algorithms.arrays;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class App {	
 	public static void main(String[] args) {
 		
-		List<String> names = new ArrayList<>();
-		names.add("Kevin");
-		names.add("Daniel");
-		names.add("Adam");
-		names.add("Ana");
-		
-//		names.remove(0);
-//		System.out.println(names.get(0));
-		
-		System.out.println(names.size());
-		for (String name: names) {
-			System.out.println(name);
-		}
-		
-		
-		int[] nums = new int[10];
-		
-		for (int i=0; i<10; i++) {
-			nums[i] = i;
-		}
-
-		nums[0] = 100;
+//		List<String> names = new ArrayList<>();
+//		names.add("Kevin");
+//		names.add("Daniel");
+//		names.add("Adam");
+//		names.add("Ana");
+//		
+////		names.remove(0);
+////		System.out.println(names.get(0));
+//		
+//		System.out.println(names.size());
+//		for (String name: names) {
+//			System.out.println(name);
+//		}
+//		
+//		
+//		int[] nums = new int[10];
+//		
+//		for (int i=0; i<10; i++) {
+//			nums[i] = i;
+//		}
+//
+//		nums[0] = 100;
 		
 //		for (int i=0; i<10; i++) {
 //			System.out.println(nums[i]);
@@ -38,5 +39,11 @@ public class App {
 //				System.out.println("값 찾음 인덱스: " + i);
 //			}
 //		}
+		
+		int[] nums = {1, 2, 3, 4};
+		
+		ReverseArrayProblem problem = new ReverseArrayProblem();
+		
+		System.out.println(Arrays.toString(problem.solve(nums)));
 	}
 }
---
+++ algorithms/src/algorithms/arrays/ReverseArrayProblem.java
@@ -0,0 +1,23 @@
+package algorithms.arrays;
+
+public class ReverseArrayProblem {
+	public int[] solve(int[] nums) {
+		int lowIndex = 0;
+		int highIndex = nums.length - 1;
+		
+		while(lowIndex <= highIndex) {
+			swap(nums, lowIndex, highIndex);
+			lowIndex++;
+			highIndex--;
+		}
+		
+		return nums;
+	}
+
+	private void swap(int[] nums, int index1, int index2) {
+		int temp = nums[index1];
+		nums[index1] = nums[index2];
+		nums[index2] = temp;
+	}
+}
+ 
