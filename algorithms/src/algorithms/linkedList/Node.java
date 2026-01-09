9307216d706c2b32a28cbb5937ea0eb695364fea
Jeon Seung Hwan <fabronjeon@naver.com>
2026-01-09 15:41:19 +0900

    Section5-Linked list implementation I

---
+++ algorithms/src/algorithms/linkedList/Node.java
@@ -0,0 +1,28 @@
+package algorithms.linkedList;
+
+public class Node<T extends Comparable<T>> {
+	private T data;
+	// LinkedList가 Array보다 더 많은 메모리를 필요로 하는 이유.
+	private Node<T> nextNode;
+	public Node(T data) {
+		this.data = data;
+	}
+	
+	public T getData() {
+		return data;
+	}
+	public void setData(T data) {
+		this.data = data;
+	}
+	public Node<T> getNextNode() {
+		return nextNode;
+	}
+	public void setNextNode(Node<T> nextNode) {
+		this.nextNode = nextNode;
+	}
+
+	@Override
+	public String toString() {
+		return "Node [data=" + data + "]";
+	}
+}
