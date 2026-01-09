64ed25b76b42e0313973cca3be42b7574f29a34b
Jeon Seung Hwan <fabronjeon@naver.com>
2026-01-09 14:46:17 +0900

    Section4-practice4

---
+++ algorithms/src/algorithms/arrays/IntegerReverse.java
@@ -0,0 +1,51 @@
+package algorithms.arrays;
+
+import java.util.LinkedList;
+
+public class IntegerReverse {
+	public static void main(String[] args) {
+		int n = 1234;
+		
+		IntegerReverse a = new IntegerReverse();
+		System.out.println(a.reverse(n));
+		System.out.println(a.reverse2(n));
+	}
+	
+	public int reverse(int n) {
+		int reversed = 0;
+		int remainder = 0;
+		
+		LinkedList<Integer> numbers = new LinkedList<>();
+		
+		int i = 1;
+		int quota = 0;
+		while (true) {
+			remainder = n % (i * 10);
+			quota = remainder / i;
+			numbers.add(quota);
+			if (remainder == n) break;
+			i *= 10;
+			quota = 0;
+		}
+
+		int numbersSize = numbers.size();
+		for (int j=0; j < numbersSize; j++) {
+			int k = numbers.removeLast();
+			reversed = (int) (reversed + k*Math.pow(10, j));
+		}
+		
+		return reversed;
+	}
+	
+	public int reverse2(int n) {
+		int reversed = 0;
+		
+		while (n != 0) {
+			int digit = n % 10;
+			reversed = reversed * 10 + digit;
+			n /= 10;
+		}
+		
+		return reversed;
+	}
+}
--- algorithms/src/algorithms/arrays/PalindromeProblem.java
+++ algorithms/src/algorithms/arrays/PalindromeProblem.java
@@ -8,6 +8,8 @@ public class PalindromeProblem {
 		PalindromeProblem palindrome = new PalindromeProblem();
 		System.out.println(palindrome.solve(s1));
 		System.out.println(palindrome.solve(s2));
+		
+		System.out.println(palindrome.solve2(s1, 0, s1.length()-1));
 	}
 	
 	public static boolean solve(String s) {
@@ -19,5 +21,11 @@ public class PalindromeProblem {
 			i++; j--;
 		}
 		return true;
+	}
+	
+	public static boolean solve2(String s, int left, int right) {
+		if (left >= right) return true;
+		if (s.charAt(left) != s.charAt(right)) return false;
+		return solve2(s, left+1, right-1);
 	}
 }
