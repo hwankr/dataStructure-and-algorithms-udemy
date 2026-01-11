38c971c784c615b904fe88093dc9817f48bf263e
Jeon Seung Hwan <fabronjeon@naver.com>
2026-01-11 10:44:54 +0900

    Section5-21

--- algorithms/src/algorithms/linkedList/App.java
+++ algorithms/src/algorithms/linkedList/App.java
@@ -2,15 +2,23 @@ package algorithms.linkedList;
 
 public class App {
 	public static void main(String[] args) {
-		LinkedList<String> names = new LinkedList<>();
 		
-		names.insert("Adam");
-		names.insert("Daniel");
-		names.insert("Ana");
-		names.traverse();
+		LinkedList<Person> people = new LinkedList<>();
 		
-		names.remove("Michael");
-		names.traverse();
+//		names.insert("Adam");
+//		names.insert("Daniel");
+//		names.insert("Ana");
+//		names.traverse();
+//		
+//		names.remove("Michael");
+//		names.traverse();
 		
+		Person p = new Person(23, "Adam");
+		people.insert(p);
+		people.insert(new Person(34, "Daniel"));
+		people.insert(new Person(56, "Michael"));
+		
+		people.remove(p);
+		people.traverse();
 	}
 }
