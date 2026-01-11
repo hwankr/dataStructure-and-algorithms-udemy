0be4060889ccd6bbdf09baa99366d628b5fabe71
Jeon Seung Hwan <fabronjeon@naver.com>
2026-01-11 11:53:55 +0900

    Section6-27

--- algorithms/src/algorithms/doublyLinkedList/DoublyLinkedList.java
+++ algorithms/src/algorithms/doublyLinkedList/DoublyLinkedList.java
@@ -7,6 +7,42 @@ public class DoublyLinkedList<T> {
 	private Node<T> tail;
 	private int size;
 	
+	public T getFirst() {
+		if (this.head == null) 
+			throw new RuntimeException("List is empty");
+		
+		T removeNode = this.head.getData();
+		
+		this.head = this.head.getNext();
+		if (this.head != null)
+			this.head.setPrevious(null);
+		else {
+			this.tail = null;
+		}
+		
+		this.size--;
+		
+		return removeNode;
+	}
+	
+	public T getLast() {
+		if (this.tail == null) 
+			throw new RuntimeException("List is empty");
+		
+		T removeNode = this.tail.getData();
+		
+		this.tail = this.tail.getPrevious();
+		if (this.tail != null)
+			this.tail.setNext(null);
+		else {
+			this.head = null;
+		}
+		
+		this.size--;
+		
+		return removeNode;
+	}
+	
 	// O(1)
 	public void addFirst(T data) {
 		Node<T> newNode = new Node<>(data);
@@ -36,5 +72,9 @@ public class DoublyLinkedList<T> {
 		}
 		
 		this.size++;
+	}
+	
+	public void removeFirst(T data) {
+		
 	}
 }
