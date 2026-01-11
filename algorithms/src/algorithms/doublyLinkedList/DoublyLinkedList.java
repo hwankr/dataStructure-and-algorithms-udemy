b4ab241acce69a3a2eb5b4c90305154cf76fa353
Jeon Seung Hwan <fabronjeon@naver.com>
2026-01-11 11:27:59 +0900

    Section6-26

--- algorithms/src/algorithms/doublyLinkedList/DoublyLinkedList.java
+++ algorithms/src/algorithms/doublyLinkedList/DoublyLinkedList.java
@@ -4,5 +4,37 @@ public class DoublyLinkedList<T> {
 	// Fist Node
 	private Node<T> head;
 	// Last node
-	private Node<T> last;
+	private Node<T> tail;
+	private int size;
+	
+	// O(1)
+	public void addFirst(T data) {
+		Node<T> newNode = new Node<>(data);
+		
+		if (this.head == null) {
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			newNode.setNext(this.head);
+			this.head.setPrevious(newNode);
+			this.head = newNode;
+		}
+		
+		this.size++;
+	}
+	
+	public void addLast(T data) {
+		Node<T> newNode = new Node<>(data);
+		
+		if (this.tail == null) {
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			this.tail.setNext(newNode);
+			newNode.setPrevious(this.tail);
+			this.tail = newNode;
+		}
+		
+		this.size++;
+	}
 }
