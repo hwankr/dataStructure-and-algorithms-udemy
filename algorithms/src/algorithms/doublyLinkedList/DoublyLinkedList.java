3e0d1fb10c1d6a18112fffba7b6cb8fa16846d51
Jeon Seung Hwan <fabronjeon@naver.com>
2026-01-11 11:20:14 +0900

    Section6-25

---
+++ algorithms/src/algorithms/doublyLinkedList/DoublyLinkedList.java
@@ -0,0 +1,8 @@
+package algorithms.doublyLinkedList;
+
+public class DoublyLinkedList<T> {
+	// Fist Node
+	private Node<T> head;
+	// Last node
+	private Node<T> last;
+}
---
+++ algorithms/src/algorithms/doublyLinkedList/Node.java
@@ -0,0 +1,36 @@
+package algorithms.doublyLinkedList;
+
+public class Node<T> {
+	private T data;
+	private Node<T> next;
+	private Node<T> previous;
+		
+	public Node(T data) {
+		this.data = data;
+	}
+
+	public T getData() {
+		return this.data;
+	}
+	
+	public Node<T> getNext() {
+		return this.next;
+	}
+	
+	public Node<T> getPrevious() {
+		return this.previous;
+	}
+	
+	public void setNext(Node<T> next) {
+		this.next = next;
+	}
+	
+	public void setPrevious(Node<T> previous) {
+		this.previous = previous;
+	}
+	
+	@Override
+	public String toString() {
+		return data.toString();
+	}
+}
